acaa4e763a2436313bab788afe059d2856b92fef
Guilherme-Espinosa <luiz5913@live.com>
2023-05-26 16:00:47 -0300

    First component created - card-news - he says - Hello world

---
+++ src/component/cardNews.js
@@ -0,0 +1,19 @@
+class cardNews extends HTMLElement {
+    constructor (){
+        super();
+
+        const shadow = this.attachShadow({mode: "open"});
+        shadow.innerHTML = "<h1>Hello World</h1>"
+    }
+}
+
+customElements.define('card-news', cardNews);
+
+
+
+
+
+
+
+
+
