7e66e14a18b2cdb01b32a07fd0de90c2b24f158d
Guilherme-Espinosa <luiz5913@live.com>
2023-05-26 22:08:54 -0300

    criação do segundo component, de uma prop e estudo da anatomia e vantagens de um component

---
+++ src/components/TituloDinamico.js
@@ -0,0 +1,28 @@
+// Criando um component
+// É sempre necessário chamar o "constructor" e o método "super"
+
+class TituloDinamico extends HTMLElement {
+    constructor() {
+        super();
+
+        const shadow = this.attachShadow({ mode: "open" });
+
+        // TODO: Criar base do component
+        const componentRoot = document.createElement("h1");
+        componentRoot.textContent = this.getAttribute("titulo");
+
+        // TODO: Estilizar o component
+        const style = document.createElement("style");
+        style.textContent = `
+            h1{
+                color: red;
+            }
+        `
+
+        // Enviar para a shadow
+        shadow.appendChild(componentRoot);
+        shadow.appendChild(style);
+    }
+}
+
+customElements.define('titulo-dinamico', TituloDinamico)
